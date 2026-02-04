fc9a5a8fd822fdfcfff5b8f3bf5b4fd54e194eb4
roylkng <rajan.kadeval@gmail.com>
2026-02-04 20:34:38 +0530

    image

---
+++ apps/desktop/.vite/deps/@radix-ui_react-slot.js
@@ -0,0 +1,18 @@
+import {
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+} from "./chunk-A3JMCUOA.js";
+import "./chunk-QGVOWARK.js";
+import "./chunk-LD7CC62Z.js";
+import "./chunk-GJSZFCZH.js";
+import "./chunk-4MBMRILA.js";
+export {
+  Slot as Root,
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+};
+//# sourceMappingURL=@radix-ui_react-slot.js.map
---
+++ apps/desktop/.vite/deps/@tauri-apps_api_core.js
@@ -0,0 +1,28 @@
+import {
+  Channel,
+  PluginListener,
+  Resource,
+  SERIALIZE_TO_IPC_FN,
+  addPluginListener,
+  checkPermissions,
+  convertFileSrc,
+  invoke,
+  isTauri,
+  requestPermissions,
+  transformCallback
+} from "./chunk-7KISBTON.js";
+import "./chunk-4MBMRILA.js";
+export {
+  Channel,
+  PluginListener,
+  Resource,
+  SERIALIZE_TO_IPC_FN,
+  addPluginListener,
+  checkPermissions,
+  convertFileSrc,
+  invoke,
+  isTauri,
+  requestPermissions,
+  transformCallback
+};
+//# sourceMappingURL=@tauri-apps_api_core.js.map
